4f72ac6d358d1df7456e6caf7294a5b3aaa2e6ee
xiaolong <gagaxiaolong@gmail.com>
2021-10-15 21:52:14 +0800

    error process

--- scripts/download_pdf.py
+++ scripts/download_pdf.py
@@ -19,21 +19,39 @@ col_papers = db.papers
 paper_total = col_papers.count()
 pdf_count = len(glob(os.path.join(PDF_PATH, "*/*.pdf")))
 
-for paper in col_papers.find():
+def download_pdf(paper):
+    """
+    If the downloads fails, then download the previous version.
+    """
+    global pdf_count
     ym = paper["id"].split(".")[0]
     ym_path = os.path.join(PDF_PATH, ym)
-    latest_pdf = "{}{}.pdf".format(paper["id"], paper["versions"][-1]["version"])
+    
+    for v in paper["versions"][::-1]:
 
-    if not os.path.exists(ym_path):
-        os.makedirs(ym_path)
+        pdf = "{}{}.pdf".format(paper["id"], v["version"])
+        if not os.path.exists(ym_path):
+            os.makedirs(ym_path)
 
-    if os.path.isfile(os.path.join(ym_path, latest_pdf)):
-        continue
+        if os.path.isfile(os.path.join(ym_path, pdf)):
+            continue
 
-    paper_path = "gs://arxiv-dataset/arxiv/arxiv/pdf/{}/{}".format(ym, latest_pdf)
-    try:
-        subprocess.run(["gsutil", "cp", paper_path, ym_path])
-    except Exception as e:
-        continue
-    pdf_count = pdf_count + 1
-    print("{}/{}".format(pdf_count, paper_total))
+        paper_path = "gs://arxiv-dataset/arxiv/arxiv/pdf/{}/{}".format(ym, pdf)
+        r = subprocess.run(["gsutil", "cp", paper_path, ym_path], capture_output=True)
+
+        if "No URLs matched" in r.stderr.decode():
+            print("Download fails, download the previous version.")
+            continue
+        elif "Operation completed over" in r.stderr.decode():
+            print("Download {} successful.".format(pdf))
+            pdf_count = pdf_count + 1
+            print("{}/{}".format(pdf_count, paper_total))
+            break
+        else:
+            print(r.stderr)
+
+
+for paper in col_papers.find().skip(9000):
+    
+    download_pdf(paper)
+    
